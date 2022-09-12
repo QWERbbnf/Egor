38bbfbfbbd483d9fda79e59784ac49bf51fd5418
fffffff <koroleve010@gmail.com>
2022-09-12 20:14:25 +0400

    5

---
+++ work/homework6.java
@@ -0,0 +1,23 @@
+package work;
+
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+public class homework6 {
+}
+class sequence {
+    @Contract(pure = true)
+    public static int @NotNull [] filter(int[] array, ByCondition condition) {
+        int conditionArrayLength = getConditionArrayLength(array, condition);
+
+        int[] newArray = new int[conditionArrayLength];
+        int i = 0;
+        for (int item : array);
+        return newArray;
+    }
+
+    private static int getConditionArrayLength(Object array, Object condition) {
+        final int i = 0;
+        return i;
+    }
+}
